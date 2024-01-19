472a27675b4987ceb844488697c7d55560597491
Daniel Sarmiento <josedanielsarmiento219@gmail.com>
2024-01-19 16:41:05 -0500

    Configure a basic docker image and cmake

---
+++ Dockerfile
@@ -0,0 +1,37 @@
+FROM alpine:3.14
+
+
+RUN apk update \
+  && apk upgrade 
+
+RUN apk add --no-cache \
+    clang \
+    clang-dev \
+    alpine-sdk \
+    dpkg \
+    cmake \
+    ccache \
+    python3 
+
+
+# Set the working directory
+WORKDIR /code
+
+# Copy the code source
+COPY ./src /code/src
+COPY ./test /code/test
+COPY ./CMakeLists.txt /code/CMakeLists.txt
+COPY ./utility /code/utility
+
+# Build the project dependencies oatpp
+RUN ./utility/install-oatpp-modules.sh
+
+# Build the project
+WORKDIR /code/build
+RUN cmake ..
+RUN make
+
+
+EXPOSE 8000 8000
+
+ENTRYPOINT ["./api_machine_learning-exe"]
--- app.cpp
+++ app.cpp
@@ -1,25 +1,92 @@
-#include "oatpp/web/server/HttpConnectionHandler.hpp"
+#include "iostream"
 
+// RestApi Framework
 #include "oatpp/network/Server.hpp"
+#include "oatpp/web/server/HttpConnectionHandler.hpp"
 #include "oatpp/network/tcp/server/ConnectionProvider.hpp"
+#include "oatpp/core/macro/codegen.hpp"
+// json 
+#include "oatpp/parser/json/mapping/ObjectMapper.hpp"
 
-using namespace oatpp::web::server;
+
+// including a important namespaces
+using namespace oatpp;
 using namespace oatpp::network;
+using namespace oatpp::web::server;
+using namespace oatpp::data::mapping;
+using namespace std;
+
+/* Begin DTO code-generation */
+#include OATPP_CODEGEN_BEGIN(DTO)
+
+/**
+ * Message Data-Transfer-Object
+ */
+class MessageDto : public DTO {
+
+    DTO_INIT(MessageDto, DTO /* Extends */)
+
+    DTO_FIELD(Int32, statusCode);   // Status code field
+    DTO_FIELD(String, message);     // Message field
+
+};
+
+/* End DTO code-generation */
+#include OATPP_CODEGEN_END(DTO)
+
+/**
+ * Custom Request Handler
+ */
+class Handler : public HttpRequestHandler
+{
+private:
+    shared_ptr<ObjectMapper> m_objectMapper;
+public:
+    /**
+     * Constructor with object mapper.
+     * @param objectMapper - object mapper used to serialize objects.
+     */
+    Handler(const shared_ptr<ObjectMapper>& objectMapper)
+        : m_objectMapper(objectMapper)
+    {}
+    /**
+     * Handle incoming request and return outgoing response.
+     */
+    shared_ptr<OutgoingResponse> handle(const shared_ptr<IncomingRequest> &request) override
+    {
+        auto message = MessageDto::createShared();
+        
+        message->statusCode = 1024;
+        message->message = "Hello DTO!";
+        
+        return ResponseFactory::createResponse(
+            Status::CODE_200,
+            message,
+            m_objectMapper
+        );
+    }
+};
 
 void run()
 {
+    /* Create json object mapper */
+    auto objectMapper = parser::json::mapping::ObjectMapper::createShared();
 
     /* Create Router for HTTP requests routing */
     auto router = HttpRouter::createShared();
 
-    /* Create HTTP connection handler with router */
+    // Route GET - "/hello" requests to Handler
+    router -> route("GET", "/hello", std::make_shared<Handler>(objectMapper));
+
+
+    // Create HTTP connection handler with router 
     auto connectionHandler = HttpConnectionHandler::createShared(router);
 
     /* Create TCP connection provider */
     auto connectionProvider = tcp::server::ConnectionProvider::createShared(
         {
-            "localhost", 
-            8000, 
+            "localhost",
+            8000,
             Address::IP_4
         }
     );
@@ -28,7 +95,11 @@ void run()
     Server server(connectionProvider, connectionHandler);
 
     /* Print info about server port */
-    OATPP_LOGI("MyApp", "Server running on port %s", connectionProvider->getProperty("port").getData());
+    OATPP_LOGI(
+        "MyApp",
+        "Server running on run on port %s \n",
+        connectionProvider->getProperty("port").getData()    
+    );
 
     /* Run server */
     server.run();
