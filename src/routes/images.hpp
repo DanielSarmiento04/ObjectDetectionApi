bd642b4f9dd9f390d51e8566b09f99766014c675
Daniel Sarmiento <josedanielsarmiento219@gmail.com>
2024-01-22 11:18:01 -0500

    configure open cv libs in cmake

--- src/routes/images.hpp
+++ src/routes/images.hpp
@@ -10,7 +10,16 @@
 #include "oatpp/web/mime/multipart/Reader.hpp"
 #include "oatpp/web/mime/multipart/PartList.hpp"
 #include "oatpp/web/mime/multipart/FileProvider.hpp"
+#include "oatpp/web/mime/multipart/InMemoryDataProvider.hpp"
+#include "oatpp/web/mime/multipart/TemporaryFileProvider.hpp"
 
+// OpenCV dependencies
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>    // Basic OpenCV structures (cv::Mat)
+#include <opencv2/videoio.hpp> // Video write
+
+// DTO'S
+#include "models/base_dto.hpp"
 
 // native dependencies
 #include "iostream"
@@ -37,7 +46,7 @@ public:
 public:
 
     ENDPOINT_INFO(images_root) {
-        info->summary = "Base API images endpoint";
+        info->summary = "Base API images endpoint.";
     }
     ENDPOINT("GET", m_root, images_root) {
 
@@ -46,28 +55,68 @@ public:
 
 
     ENDPOINT_INFO(analyze_image) {
-        info->summary = "Endpoint to upload file ";
+        info->summary = "Endpoint to upload file.";
     }
     ENDPOINT("POST", m_root, analyze_image,
             REQUEST(std::shared_ptr<IncomingRequest>, request) // configure the request management
-        ) 
+    ) 
     {
-        /* Prepare multipart container. */
-        auto multipart = std::make_shared<multipart::PartList>(request->getHeaders());
 
-        /* Read multipart body */
-        multipart::Reader multipartReader(multipart.get());
+        /* create multipart object */
+        multipart::PartList multipart(request->getHeaders());
 
-        /* Configure to stream part with name "part1" to file */
-        multipartReader.setPartReader("part1", multipart::createFilePartReader("./part1"));
+        /* create multipart reader */
+        multipart::Reader multipartReader(&multipart);
 
-        /* Read multipart body */
+        /* setup reader to stream parts to a temporary files by default */
+        multipartReader.setDefaultPartReader(multipart::createTemporaryFilePartReader("/tmp" /* /tmp directory */));
+
+        /* upload multipart data */
         request->transferBody(&multipartReader);
 
-        /* Print value of "part1" */
-        auto part1 = multipart->getNamedPart("part1");
-        /* Assert part is not null */
-        // OATPP_ASSERT_HTTP(part1, Status::CODE_400, "part1 is null");
+        /* get part by name */
+        auto image_file = multipart.getNamedPart("image"); 
+
+        // /* Assert part is not null */
+        if(image_file == nullptr){
+            auto error_message = BaseModel::createShared();
+            error_message->message = "Input stream file as image name not found.";
+            error_message->statusCode = 500;
+            return createDtoResponse(Status::CODE_500, error_message);
+        }
+        OATPP_LOGD("part", "name=%s, location=%s", image_file->getName()->c_str(), image_file->getPayload()->getLocation()->c_str())
+
+
+        cv::Mat image = cv::imread(image_file->getPayload()->getLocation()->c_str(), cv::IMREAD_COLOR);
+
+        cv::imwrite("./image.jpg", image);
+        // /* list all parts and locations to corresponding temporary files */
+        // auto parts = multipart.getAllParts();
+        // for(auto& p : parts) {
+        //     OATPP_LOGD("part", "name=%s, location=%s", p->getName()->c_str(), p->getPayload()->getLocation()->c_str())
+        // }
+
+        // /* Prepare multipart container. */
+        // auto multipart = std::make_shared<multipart::PartList>(request->getHeaders());
+
+        // /* Read multipart body */
+        // multipart::Reader multipartReader(multipart.get());
+
+        // /* Configure to stream part with name "part1" to file */
+        // multipartReader.setPartReader("image", multipart::createInMemoryPartReader(256 /* max-data-size */));
+
+
+        // /* Read multipart body */
+        // request->transferBody(&multipartReader);
+
+        // /* Print value of "part1" */
+        // auto image_file = multipart->getNamedPart("image");
+
+        // /* Assert part is not null */
+
+        // OATPP_LOGD("Multipart", "part1='%s'", image_file->getInMemoryData()->c_str());
+
+
 
         return createResponse(Status::CODE_200, "post images");
     }
